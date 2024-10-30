fae7a0c50f00ffce9b340a85c3877c2acf4bdf0d
Luis D Mora <anubis.ld@gmail.com>
2024-10-30 15:51:59 -0600

    final commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Poplin Project - QA Challenge for Poplin App: "Order Placed" Workflow
+
+## Description 
+
+This is a technical test, the main idea of this is recreate the manual testing documentation and implement this documentation in automated test cases with cypress framework
+
+## Test Execution
+
+Please run this test cases with the Terminal with the command:
+
+npx cypress run
+
+## Project Structure
+
+This project has the structure based in 2 different part:
+
+- Spec.cy files, these are located into the `e2e` folder and this files will be the files to be executed.
+- Command files, these are located into the `support` folder and this files have the functions implemented into the Spec files.
+- It's important to take in mind, into the support folders on every command file can added a specific constant files to handle in a easier way if it is necessary.
+
+## About the author
+
+Luis D. Mora
+Software Development Engineer in Test (SDET)
+With extensive experience (+11 years) in both manual and automated testing, Luis has dedicated his career to ensuring software quality and enhancing testing processes.
--- cypress/e2e/new-orders/new-orders-spec.cy.js
+++ cypress/e2e/new-orders/new-orders-spec.cy.js
@@ -17,25 +17,25 @@ describe("E2E test - New orders", () => {
     cy.logOutUser();
   });
 
-  it("TC-004 | User Starts a new Order with a new profile for a Large Bag, basic coverage and Express delivery.", () => {
+  it("TC-004 | User Starts a new Order with an exist profile for a Small Bag, basic coverage and Express delivery.", () => {
     cy.logInUser();
-    cy.startNewOrder("Express Delivery", "#stepper-small", "Premium");
+    cy.startNewOrder("Express Delivery", "#stepper-small", "Basic");
     cy.logOutUser();
   });
 
-  it("TC-005 | User Starts a new Order With an exist profile for a Small Bag, basic coverage and Express delivery.", () => {
+  it("TC-005 | User Starts a new Order With an exist profile for a Small Bag, Premium coverage and Express delivery.", () => {
     cy.logInUser();
     cy.startNewOrder("Express Delivery", "#stepper-regular", "Premium");
     cy.logOutUser();
   });
 
-  it("TC-006 | User Starts a new Order With an exist profile for a Regular Bag, Premium+ coverage and Express delivery.", () => {
+  it("TC-006 | User Starts a new Order With an exist profile for a Large Bag, Premium+ coverage and Express delivery.", () => {
     cy.logInUser();
-    cy.startNewOrder("Express Delivery", "#stepper-large", "Premium");
+    cy.startNewOrder("Express Delivery", "#stepper-large", "Premium+");
     cy.logOutUser();
   });
 
-  it("TC-007 | User Starts a new Order with a new profile for a Large Bag, Premium+ coverage and Express delivery.", () => {
+  it("TC-007 | User Starts a new Order with an exist profile for a Large Bag, Premium+ coverage and Express delivery.", () => {
     cy.logInUser();
     cy.startNewOrder("Express Delivery", "#stepper-small", "Premium+");
     cy.logOutUser();
